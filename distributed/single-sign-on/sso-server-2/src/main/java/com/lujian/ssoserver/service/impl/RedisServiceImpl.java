405c0172442fb6c644092e49b71bde16b64437d3
lujian <lujian282012@163.com>
2017-12-07 21:44:01 +0800

    add @sso @sso-server-2 add another sso-server

---
+++ distributed/single-sign-on/sso-server-2/src/main/java/com/lujian/ssoserver/service/impl/RedisServiceImpl.java
@@ -0,0 +1,36 @@
+package com.lujian.ssoserver.service.impl;
+
+import com.lujian.ssoserver.service.RedisService;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class RedisServiceImpl implements RedisService {
+
+    @Resource
+    StringRedisTemplate template;
+
+    @Override
+    public void ping() {
+        template.opsForList().leftPush("test", System.currentTimeMillis() + "");
+    }
+
+    @Override
+    public boolean hasLogin(String id) {
+        String val = template.opsForValue().get(id);
+        return val != null;
+    }
+
+    @Override
+    public void login(String id) {
+        template.opsForValue().set(id, id);
+    }
+
+    @Override
+    public void logout(String id) {
+        template.opsForValue().set(id, null);
+    }
+
+}
---
+++ distributed/single-sign-on/sso-server-2/src/main/java/com/lujian/ssoserver/web/filter/LoginFilter.java
@@ -0,0 +1,27 @@
+package com.lujian.ssoserver.web.filter;
+
+import com.lujian.ssoserver.service.RedisService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class LoginFilter extends HandlerInterceptorAdapter {
+
+    @Resource
+    RedisService redisService;
+
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String id = request.getSession().getId();
+        if(redisService.hasLogin(id)) {
+            return true;
+        }else {
+            response.sendRedirect("/test.do");
+            return true;
+        }
+    }
+
+}
