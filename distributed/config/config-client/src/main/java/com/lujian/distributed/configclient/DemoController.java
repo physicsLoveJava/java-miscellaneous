dd4b8fe1e9f8ef864819263c187b49bcb4aa70da
lujian <lujian282012@163.com>
2017-12-13 20:05:25 +0800

    add @distributed @config

--- distributed/config/config-client/src/main/java/com/lujian/distributed/configclient/DemoController.java
+++ distributed/config/config-client/src/main/java/com/lujian/distributed/configclient/DemoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-    @Value("${dev}")
+    @Value("${s}")
     private String dev;
 
     @GetMapping("/dev")
