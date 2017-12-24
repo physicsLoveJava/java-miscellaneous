59e3069a305fb06ed1aa640790afb58643d195f0
lujian <lujian282012@163.com>
2017-12-24 14:13:42 +0800

    add @java-sources thread && thread-local

--- distributed/single-sign-on/spring-transaction/src/main/java/com/lujian/spring/transaction/annotation/Right.java
+++
@@ -1,7 +0,0 @@
-package com.lujian.spring.transaction.annotation;
-
-/**
- * annotation for right case
- */
-public @interface Right {
-}
--- distributed/single-sign-on/spring-transaction/src/main/java/com/lujian/spring/transaction/service/UserxService.java
+++
@@ -1,8 +0,0 @@
-package com.lujian.spring.transaction.service;
-
-public interface UserxService {
-
-    void transactionalServiceMethodRequiresNew();
-
-    void transactionalServiceMethodSupports();
-}
