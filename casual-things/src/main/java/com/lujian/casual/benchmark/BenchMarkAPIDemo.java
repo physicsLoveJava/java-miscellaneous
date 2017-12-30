338685e4f40b4faa2928d13d3a4081e473b04ac5
lujian <lujian282012@163.com>
2017-12-30 18:02:11 +0800

    add @java-sources JMH demo bench mark

--- casual-things/src/main/java/com/lujian/casual/benchmark/BenchMarkAPIDemo.java
+++ casual-things/src/main/java/com/lujian/casual/benchmark/BenchMarkAPIDemo.java
@@ -1,6 +1,10 @@
 package com.lujian.casual.benchmark;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.io.File;
 import java.io.IOException;
@@ -9,17 +13,19 @@ public class BenchMarkAPIDemo {
 
     @Benchmark
     public void measureName() {
-
     }
 
     @Benchmark
     public void functionBlank() {
-
     }
 
-//    public static void main(String[] args) throws IOException {
-//        File tempFile = File.createTempFile("jmh", "compilecommand");
-//        System.out.println(tempFile.getAbsolutePath());
-//    }
+    public static void main(String[] args) throws IOException, RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(BenchMarkAPIDemo.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
 
 }
