a4eeb1ff9644249d85aa19d1363c61597b785ea0
lujian <lujian282012@163.com>
2017-12-30 16:39:09 +0800

    add @java-sources outOfMemoryError stack

---
+++ casual-things/src/main/java/com/lujian/casual/oom/Stackoom.java
@@ -0,0 +1,13 @@
+package com.lujian.casual.oom;
+
+public class Stackoom {
+
+    public static void main(String[] args) {
+        test();
+    }
+
+    private static void test() {
+        test();
+    }
+
+}
