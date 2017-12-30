e871e16a02f5eae99151b11b7986a0ca40af6fa9
lujian <lujian282012@163.com>
2017-12-30 16:13:38 +0800

    add for the workspace change

---
+++ casual-things/src/main/java/com/lujian/casual/benchmark/BenchMarkAPIDemo.java
@@ -0,0 +1,25 @@
+package com.lujian.casual.benchmark;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+import java.io.File;
+import java.io.IOException;
+
+public class BenchMarkAPIDemo {
+
+    @Benchmark
+    public void measureName() {
+
+    }
+
+    @Benchmark
+    public void functionBlank() {
+
+    }
+
+//    public static void main(String[] args) throws IOException {
+//        File tempFile = File.createTempFile("jmh", "compilecommand");
+//        System.out.println(tempFile.getAbsolutePath());
+//    }
+
+}
