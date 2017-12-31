13d41ad88d1901baa6d7260084849fe97be3b48e
lujian <lujian282012@163.com>
2017-12-31 23:32:33 +0800

    add @java-sources JMH benchmark default state by add annotation in class

---
+++ casual-things/src/main/java/com/lujian/casual/benchmark/BenchMarkDefaultState.java
@@ -0,0 +1,49 @@
+package com.lujian.casual.benchmark;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+@State(Scope.Thread)
+public class BenchMarkDefaultState {
+
+    double x = Math.PI;
+
+    @Benchmark
+    public void measure() {
+        x++;
+    }
+
+    /*
+     * ============================== HOW TO RUN THIS TEST: ====================================
+     *
+     * You can see the benchmark runs as usual.
+     *
+     * You can run this test:
+     *
+     * a) Via the command line:
+     *    $ mvn clean install
+     *    $ java -jar target/benchmarks.jar JMHSample_04 -wi 5 -i 5 -f 1
+     *    (we requested 5 warmup/measurement iterations, single fork)
+     *
+     * b) Via the Java API:
+     *    (see the JMH homepage for possible caveats when running from IDE:
+     *      http://openjdk.java.net/projects/code-tools/jmh/)
+     */
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(BenchMarkDefaultState.class.getSimpleName())
+                .warmupIterations(5)
+                .measurementIterations(5)
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+}
