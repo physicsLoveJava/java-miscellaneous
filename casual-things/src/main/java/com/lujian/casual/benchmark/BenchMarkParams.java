0b4dec42de725c880b7973bb7c52488bd4bfbb9c
lujian <lujian282012@163.com>
2018-01-03 23:43:26 +0800

    add @java-sources jmh params support

---
+++ casual-things/src/main/java/com/lujian/casual/benchmark/BenchMarkParams.java
@@ -0,0 +1,67 @@
+package com.lujian.casual.benchmark;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.BenchmarkParams;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.math.BigInteger;
+import java.util.concurrent.TimeUnit;
+
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(1)
+@State(Scope.Benchmark)
+public class BenchMarkParams {
+
+
+        /**
+         * In many cases, the experiments require walking the configuration space
+         * for a benchmark. This is needed for additional control, or investigating
+         * how the workload performance changes with different settings.
+         */
+
+        @Param({"1", "31", "65", "101", "103"})
+        public int arg;
+
+        @Param({"0", "1", "2", "4", "8", "16", "32"})
+        public int certainty;
+
+        @Benchmark
+        public boolean bench() {
+            return BigInteger.valueOf(arg).isProbablePrime(certainty);
+        }
+
+    /*
+     * ============================== HOW TO RUN THIS TEST: ====================================
+     *
+     * Note the performance is different with different parameters.
+     *
+     * You can run this test:
+     *
+     * a) Via the command line:
+     *    $ mvn clean install
+     *    $ java -jar target/benchmarks.jar JMHSample_27
+     *
+     *    You can juggle parameters through the command line, e.g. with "-p arg=41,42"
+     *
+     * b) Via the Java API:
+     *    (see the JMH homepage for possible caveats when running from IDE:
+     *      http://openjdk.java.net/projects/code-tools/jmh/)
+     */
+
+        public static void main(String[] args) throws RunnerException {
+            Options opt = new OptionsBuilder()
+                    .include(BenchmarkParams.class.getSimpleName())
+//                .param("arg", "41", "42") // Use this to selectively constrain/override parameters
+                    .build();
+
+            new Runner(opt).run();
+        }
+
+}
