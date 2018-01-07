54fa0e52b6a9f932292d49e425a32c65745fd96a
lujian <lujian282012@163.com>
2018-01-07 22:15:27 +0800

    add @gc object age in 1

---
+++ casual-things/src/main/java/com/lujian/casual/gc/AllocationTenuringThreshold.java
@@ -0,0 +1,28 @@
+package com.lujian.casual.gc;
+
+public class AllocationTenuringThreshold {
+
+    private static int _1mb = 1024 * 1024;
+
+    /**
+     * -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails
+        -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold*l
+     -XX:+PrintTenuringDistribution
+     * @param args
+     * @throws InterruptedException
+     */
+    public static void main(String[] args) throws InterruptedException {
+        byte[] alloc1, alloc2, alloc3;
+        alloc1 = new byte[_1mb / 4];
+        Thread.sleep(5000);
+        alloc2 = new byte[_1mb * 4];
+        Thread.sleep(5000);
+        alloc3 = new byte[_1mb * 4];
+        Thread.sleep(5000);
+        alloc3 = null;
+        Thread.sleep(5000);
+        alloc3 = new byte[_1mb * 4];
+        Thread.sleep(5000);
+    }
+
+}
