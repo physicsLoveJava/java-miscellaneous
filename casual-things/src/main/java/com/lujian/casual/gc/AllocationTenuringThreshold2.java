d350159f7ad7766af226f1bcf589d4ccf091115f
lujian <lujian282012@163.com>
2018-01-07 22:26:34 +0800

    add @gc dynamic allocation

---
+++ casual-things/src/main/java/com/lujian/casual/gc/AllocationTenuringThreshold2.java
@@ -0,0 +1,25 @@
+package com.lujian.casual.gc;
+
+public class AllocationTenuringThreshold2 {
+
+    private static int _1mb = 1024 * 1024;
+
+    /**
+     * -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails
+        -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=15
+     -XX:+PrintTenuringDistribution
+     * @param args
+     * @throws InterruptedException
+     */
+    public static void main(String[] args) throws InterruptedException {
+        byte[] alloc1, alloc2, alloc3, alloc4;
+        alloc1 = new byte[_1mb / 4];
+        alloc2 = new byte[_1mb / 4];
+        alloc3 = new byte[_1mb * 4];
+        alloc4 = new byte[_1mb * 4];
+        alloc4 = null;
+        alloc4 = new byte[_1mb * 4];
+        Thread.sleep(5000);
+    }
+
+}
