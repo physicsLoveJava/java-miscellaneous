7899290a217adbad2ca8da06e69e88f391f114f0
lujian <lujian282012@163.com>
2018-01-07 21:43:33 +0800

    add @gc reference counting

---
+++ casual-things/src/main/java/com/lujian/casual/gc/ReferenceCountGC.java
@@ -0,0 +1,37 @@
+package com.lujian.casual.gc;
+
+public class ReferenceCountGC {
+
+    private Object ref;
+
+    private byte[] bytes = new byte[1024 * 1024 * 100];
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread.sleep(5000);
+        ReferenceCountGC a = new ReferenceCountGC();
+        ReferenceCountGC b = new ReferenceCountGC();
+
+        a.ref = b;
+        b.ref = a;
+
+        Thread.sleep(5000);
+
+        System.out.println("1 gc");
+        System.gc();
+
+        Thread.sleep(5000);
+
+        a = null;
+        b = null;
+
+        System.out.println("2 gc");
+
+        Thread.sleep(5000);
+        System.gc();
+
+        Thread.sleep(10000);
+
+    }
+
+}
