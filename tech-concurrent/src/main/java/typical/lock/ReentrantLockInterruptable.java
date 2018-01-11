f09ee83c5f2a24f88cfccce89644681258fb4a95
lujian <lujian282012@163.com>
2018-01-11 09:30:54 +0800

    add @java-sources concurrent reentrant lock

---
+++ tech-concurrent/src/main/java/typical/lock/ReentrantLockInterruptable.java
@@ -0,0 +1,69 @@
+package typical.lock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockInterruptable {
+
+    private static ReentrantLock lock1 = new ReentrantLock();
+    private static ReentrantLock lock2 = new ReentrantLock();
+
+    static class Worker implements Runnable {
+
+        private ReentrantLock first;
+        private ReentrantLock second;
+
+        public Worker(ReentrantLock first, ReentrantLock second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        @Override
+        public void run() {
+            try {
+                first.lockInterruptibly();
+                System.out.println("get lock:" + first);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    second.lockInterruptibly();
+                    System.out.println("get lock:" + second);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    if(second.isHeldByCurrentThread()) {
+                        second.unlock();
+                        System.out.println("release lock:" + second);
+                    }
+                }
+                if(first.isHeldByCurrentThread()) {
+                    first.unlock();
+                    System.out.println("release lock:" + first);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(new Worker(lock1, lock2));
+        Thread t2 = new Thread(new Worker(lock2, lock1));
+        t1.start();
+        t2.start();
+
+        try {
+            Thread.sleep(1000);
+            t1.interrupt();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
