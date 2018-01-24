606fab8745fc5ced3d859ed3dac83e5c622de7ee
lujian <lujian282012@163.com>
2018-01-24 10:53:23 +0800

    add @netflix conductor class diagram

--- commons/src/main/java/com/lujian/commons/GenerateNetflixClassDiagramFile.java
+++ commons/src/main/java/com/lujian/commons/GenerateNetflixClassDiagramFile.java
@@ -6,8 +6,11 @@ public class GenerateNetflixClassDiagramFile {
 
     public static void main(String[] args) {
 
-        createClassDiagram("E:\\frameworks\\java\\blitz4j\\src\\main",
-                "commons/src/main/resources/blitz4j.txt");
+//        createClassDiagram("E:\\frameworks\\java\\blitz4j\\src\\main",
+//                "commons/src/main/resources/blitz4j.txt");
+
+        createClassDiagram("E:\\frameworks\\java\\conductor",
+                "commons/src/main/resources/conductor.txt");
 
     }
 
