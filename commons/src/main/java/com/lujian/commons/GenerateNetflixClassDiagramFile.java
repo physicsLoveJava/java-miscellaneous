f70158f4d83fc4ffa46d050c26eb6d88988f6533
lujian <lujian282012@163.com>
2018-01-24 10:51:00 +0800

    add @netflix blitz4j class diagram

---
+++ commons/src/main/java/com/lujian/commons/GenerateNetflixClassDiagramFile.java
@@ -0,0 +1,14 @@
+package com.lujian.commons;
+
+import static com.lujian.commons.GenerateClassDiagramFile.createClassDiagram;
+
+public class GenerateNetflixClassDiagramFile {
+
+    public static void main(String[] args) {
+
+        createClassDiagram("E:\\frameworks\\java\\blitz4j\\src\\main",
+                "commons/src/main/resources/blitz4j.txt");
+
+    }
+
+}
