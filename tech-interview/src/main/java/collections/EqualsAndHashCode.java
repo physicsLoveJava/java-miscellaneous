3bb54c947b8300074306daab85213f236e7a11cf
lujian <lujian282012@163.com>
2018-03-21 12:56:28 +0800

    add @interview object equals and hashcode add hash map examples

--- tech-interview/src/main/java/collections/EqualsAndHashCode.java
+++ tech-interview/src/main/java/collections/EqualsAndHashCode.java
@@ -1,6 +1,8 @@
 package collections;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class EqualsAndHashCode {
 
@@ -45,24 +47,35 @@ public class EqualsAndHashCode {
         }
     }
 
-    //violate set contract every element is equals
-    //can't use hash to reduce object equality
-    public static void main(String[] args) {
+    static class Test1 {
+        //violate set contract every element is equals
+        //can't use hash to reduce object equality
+        public static void main(String[] args) {
+            HashSet<Person> set = new HashSet<>(10);
+            for (int i = 0; i < 10; i++) {
+                set.add(new Person("person" + i, i));
+            }
+            for (int i = 0; i < 10; i++) {
+                set.add(new Person("person", 15));
+            }
+            Person p1 = new Person("person", 15);
+            Person p2 = new Person("person", 15);
+            System.out.println(p1 + " hascode: " + p1.hashCode());
+            System.out.println(p2 + " hascode: " + p2.hashCode());
+            System.out.println("person1 equals person2 " + p1.equals(p2));
+            for (Person person : set) {
+                System.out.println(person);
+            }
+        }
+    }
 
-        HashSet<Person> set = new HashSet<>(10);
-        for (int i = 0; i < 10; i++) {
-            set.add(new Person("person" + i, i));
-        }
-        for (int i = 0; i < 10; i++) {
-            set.add(new Person("person", 15));
-        }
-        Person p1 = new Person("person", 15);
-        Person p2 = new Person("person", 15);
-        System.out.println(p1 + " hascode: " + p1.hashCode());
-        System.out.println(p2 + " hascode: " + p2.hashCode());
-        System.out.println("person1 equals person2 " + p1.equals(p2));
-        for (Person person : set) {
-            System.out.println(person);
+    static class Test2 {
+        public static void main(String[] args) {
+            Map<Person, Person> map = new HashMap<>();
+            Person p1 = new Person("person", 15);
+            Person p2 = new Person("person", 15);
+            map.put(p1, p1);
+            System.out.println(map.get(p2));
         }
     }
 }
