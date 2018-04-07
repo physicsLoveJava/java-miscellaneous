1b96a75a0e2d0997c5181235ab4eb56f80128799
lujian <lujian282012@163.com>
2018-04-07 10:24:33 +0800

    add @concurrent jol sample

---
+++ casual-things/src/main/java/com/lujian/casual/jol/Basic.java
@@ -0,0 +1,27 @@
+package com.lujian.casual.jol;
+
+import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.vm.VM;
+
+import static java.lang.System.out;
+
+public class Basic {
+
+    /*
+     * This sample showcases the basic field layout.
+     * You can see a few notable things here:
+     *   a) how much the object header consumes;
+     *   b) how fields are laid out;
+     *   c) how the external alignment beefs up the object size
+     */
+
+    public static void main(String[] args) throws Exception {
+        out.println(VM.current().details());
+        out.println(ClassLayout.parseClass(A.class).toPrintable());
+    }
+
+    public static class A {
+        boolean f;
+    }
+
+}
