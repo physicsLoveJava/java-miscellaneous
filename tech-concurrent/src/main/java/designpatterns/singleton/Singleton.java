0b0b5b20c8be7d6e3982d25d1b83763df3c70ba1
lujian <lujian282012@163.com>
2018-04-29 23:41:46 +0800

    add @interview 单例模式 泛型单例模式？？

---
+++ tech-concurrent/src/main/java/designpatterns/singleton/Singleton.java
@@ -0,0 +1,16 @@
+package designpatterns.singleton;
+
+public class Singleton {
+
+    private Singleton() {
+    }
+
+    private static class SingletonHolder {
+        private static final Singleton instance = new Singleton();
+    }
+
+    public static Singleton getInstance() {
+        return SingletonHolder.instance;
+    }
+
+}
