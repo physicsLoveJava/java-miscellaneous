03f49fe8b98cec3397284c0878bbb1065ade00fd
lujian <lujian282012@163.com>
2018-04-30 13:24:30 +0800

    add @interview traditional server

--- tech-interview/src/main/java/io/TcpClient.java
+++ tech-interview/src/main/java/io/TcpClient.java
@@ -1,4 +1,30 @@
 package io;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 public class TcpClient {
+
+    public void start() {
+        Socket socket = new Socket();
+        try {
+            socket.connect(new InetSocketAddress(8000));
+            PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+            pw.println("ping....");
+            pw.flush();
+            pw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            new TcpClient().start();
+            Thread.sleep(1000);
+        }
+    }
 }
--- tech-interview/src/main/java/io/TcpServer.java
+++ tech-interview/src/main/java/io/TcpServer.java
@@ -1,4 +1,32 @@
 package io;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class TcpServer {
+
+    public void start() {
+        try {
+            ServerSocket ss = new ServerSocket(8000);
+            while (true) {
+                Socket socket = ss.accept();
+                BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    System.out.println(line);
+                }
+                br.close();
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new TcpServer().start();
+    }
+
 }
